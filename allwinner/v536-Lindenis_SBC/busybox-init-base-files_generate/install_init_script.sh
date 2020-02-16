ee28378ced555915e3bf856e264863ad40f85262
Given He <given@lindeni.com>
2020-02-16 15:17:18 +0800

    get the camera sensor config from menuconfig

--- allwinner/v536-Lindenis_SBC/busybox-init-base-files_generate/install_init_script.sh
+++ allwinner/v536-Lindenis_SBC/busybox-init-base-files_generate/install_init_script.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
 pwd=$(dirname $0)
+conf=$TINA_BUILD_TOP/.config
 
 S00mpp="S00mpp_imx317"
+
+eval $(awk '/CONFIG_imx278/' $conf)
+if [ "$CONFIG_imx278" = "y" ]; then
+	S00mpp="S00mpp_imx278"
+fi
+
+eval $(awk '/CONFIG_imx307/' $conf)
+if [ "$CONFIG_imx307" = "y" ]; then
+	S00mpp="S00mpp_imx307"
+fi
+
+eval $(awk '/CONFIG_imx386/' $conf)
+if [ "$CONFIG_imx386" = "y" ]; then
+	S00mpp="S00mpp_imx386"
+fi
 
 install -m 777 $pwd/rcK $1/etc/init.d
 install -m 777 $pwd/${S00mpp} $1/etc/init.d/S00mpp
